2bc5b684c8e925fa1c9c61dac1602a1b0532af6e
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2021-11-17 14:42:13 -0700

    pg

--- app.py
+++ app.py
@@ -54,11 +54,11 @@ def load_user(user_id):
 def index():
     if current_user.is_authenticated:
         return (
-            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            "<p>Hello, {}! You're logged in as {}! Email: {}</p>"
             "<div><p>Google Profile Picture:</p>"
             '<img src="{}" alt="Google profile pic"></img></div>'
             '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email, current_user.profile_pic
+                current_user.name, current_user.role, current_user.email, current_user.profile_pic
             )
         )
     else:
@@ -122,12 +122,15 @@ def callback():
         return "User email not available or not verified by Google.", 400
 
     user = User(
-    id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    id_=unique_id, name=users_name, email=users_email, profile_pic=picture, role=User.DEFAULT_ROLE
     )
 
     # Doesn't exist? Add it to the database.
-    if not User.get(unique_id):
-        User.create(unique_id, users_name, users_email, picture)
+    dbUser = User.get(unique_id)
+    if not dbUser:
+        User.create(user.id, user.name, user.email, user.profile_pic, user.role)
+    else:
+        user = dbUser
 
     # Begin user session by logging the user in
     login_user(user)
--- schema.sql
+++ schema.sql
@@ -1,6 +1,1 @@
-CREATE TABLE user (
-  id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  profile_pic TEXT NOT NULL
-);
+CREATE TABLE usertable (id TEXT PRIMARY KEY, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, profile_pic TEXT NOT NULL, role TEXT);
